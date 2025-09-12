be9bcda9b48795d473a38fc280eed403ab9239d2
AlekseyPanas <panasaleksey3@gmail.com>
2025-09-11 23:40:24 -0700

    Plan data structures

--- day_planner/src/App.tsx
+++ day_planner/src/App.tsx
@@ -1,15 +1,20 @@
-import { useState } from "react";
+import {useEffect, useState} from "react";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
-import Timeline from "./components/Timeline";
 
 function App() {
 
-  return (
+    const [state, setState] = useState([]);
+
+    useEffect(() => {
+
+    }, []);
+
+    return (
     <div className="container">
         <h1> TEST </h1>
     </div>
-  );
+    );
 }
 
 export default App;
---
+++ day_planner/src/state_layers/adapter_interface.ts
@@ -0,0 +1,80 @@
+export enum ContributorType {
+    SUBGOAL,
+    STRATEGY,
+    SCHEDULING
+}
+
+// Different functions of this type take a point in time and generate a relative actual time based on some potentially additional input
+export interface RelativeTimeGenerator {
+    get_relative_unix_time(relative_base_unix_time: number): number;
+}
+
+// Gets the idx-th timestamp in a pattern starting from a given lower bound, or NULL if out of bounds by upper bound
+export interface RecurrencePattern {
+    get_ith_timestamp(lower_bound_unix: number, upper_bound_unix: number, idx: number): number | null;
+}
+
+// Goal data which doesn't change across recurrence spawns
+export interface StaticGoalDat {
+    name: string;
+    desc: string | null;
+    timebased_criteria_seconds: number | null;
+    ect_seconds: number | null;
+    contributes_to: Set<number> | null;
+    contributor_type: ContributorType | null;
+}
+
+// Existing goal instance
+export interface Goal {
+    id: number;
+    static_data: StaticGoalDat;
+    timebased_progress_seconds: number | null;
+    starttime_unix: number;
+    deadline_unix: number;
+    subgoal_set: Set<number> | null;
+    is_subgoal_set_completing: boolean | null;
+    strategy_set: Set<number> | null;
+    scheduling_set: Set<number> | null;
+}
+
+// Template for generating a recurrence relative to a window
+export interface RecurrenceTemplate {
+    static_data: StaticGoalDat;
+
+    subgoal_set: Set<GoalTemplate | RecurrenceTemplate>
+    strategy_set: Set<GoalTemplate | RecurrenceTemplate>
+
+    rec_lower_bound_unix: number;  // These bounds should be set to parent goal time range if one is present
+    rec_upper_bound_unix: number | null;
+
+    rec_pattern: RecurrencePattern;
+    starttime_generator: RelativeTimeGenerator;
+    deadline_generator: RelativeTimeGenerator;
+}
+
+// Template for generating a goal relative to a time
+export interface GoalTemplate {
+    static_data: StaticGoalDat;
+
+    subgoal_set: Set<GoalTemplate | RecurrenceTemplate>
+    strategy_set: Set<GoalTemplate | RecurrenceTemplate>
+
+    starttime_rel: RelativeTimeGenerator;
+    deadline_rel: RelativeTimeGenerator;
+}
+
+// Existing recurrence instance
+export interface Recurrence {
+    id: number;
+    template: RecurrenceTemplate;
+    is_generated(idx: number): boolean;
+    link_goal(idx: number, goal_id: number): void;
+    get_linked_goal_id(idx: number): number | null;
+    unlink_goal(idx: number): void;
+    set_generated(idx: number, is_generate: boolean): void;
+}
+
+export interface AppState {
+    goals: Goal[];
+    recurrences: Recurrence[];
+}
