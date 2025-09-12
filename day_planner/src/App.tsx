8b855f0d595e6f39dcca32ea6781e90dfc11042c
AlekseyPanas <panasaleksey3@gmail.com>
2025-09-12 14:37:06 -0700

    make timeline lines :D

--- day_planner/src/App.tsx
+++ day_planner/src/App.tsx
@@ -31,10 +31,27 @@ function App({ stateLayer }: AppProps) {
         </div>
 
         <div className="main-grid" style={{width:'80%', height:'100%', margin: 0, padding: 0}}>
-            <div className="main-grid-top-half" style={{display: "flex", height: '70%'}}>
-                <div className="main-grid-left-bar" style={{width:'10%', height:'100%', margin: 0, padding: 0, backgroundColor: 'rgb(179,179,179)'}}>
 
+            <div className="main-grid-top-half" style={{display: "flex", height: '70%'}}>
+
+                <div className="main-grid-left-bar" style={{width: '10%', height: '100%', margin: 0, padding: 0, backgroundColor: 'rgb(179,179,179)'}}>
+                    <div className="main-grid-day-title-container" style={{height: "5%", outline: "1px solid"}}>
+
+                    </div>
+                    <div className="main-grid-day-time-container" style={{position: "relative", height:'75%', outline: "1px solid"}}>
+                        {Array.from(Array(hourRange[1] - hourRange[0] + 1).keys()).map((i) => {
+                            const total = hourRange[1] - hourRange[0];
+                            return <p key={i} style={{width:'100%', position: 'absolute',
+                                top: ((i / total) * 100).toString() + "%", left: 0, transform: "translateY(-50%)", color: "black", padding: 0, margin: 0, textAlign: 'right'}}>
+                                {i + hourRange[0] + ":00"}
+                            </p>;
+                        })}
+                    </div>
+                    <div className="main-grid-day-overflow-container" style={{height: "20%", outline: "1px solid"}}>
+
+                    </div>
                 </div>
+
                 <div className="main-grid-days-container" style={{display: "flex", width: "90%", justifyContent: "space-between"}}>
                     <div className="main-grid-day-col" style={{
                         flex: 1,
@@ -44,25 +61,49 @@ function App({ stateLayer }: AppProps) {
                         padding: 0,
                         backgroundColor: 'rgb(232,232,232)'
                     }}>
-                        <div className="goal-block" style={{
-                            backgroundColor: "rgb(168,244,208)",
-                            paddingLeft: "5px",
-                            paddingRight: "5px",
-                            borderRadius: "5px",
-                            fontFamily: "sans-serif",
-                            height: "25px",
-                            overflow: "hidden",
-                            borderTop: "solid 2px",
-                            borderBottom: "dashed 1px",
-                            borderRight: "dashed 1px",
-                            borderLeft: "dashed 1px",
-                            position: "absolute",
-                            top: "5%",
-                            left: '50%',
-                            transform: 'translateX(-50%)'
+                        <div className="main-grid-day-title-container" style={{
+                            height: "5%",
+                            outline: "1px solid",
+                            textAlign: "center",
+                            fontFamily: "sans-serif"
                         }}>
-                            Goal Name
+                            <h2 style={{padding: 0, margin: 0}}> Sun</h2>
+                            <h3 style={{padding: 0, margin: 0}}> 9/12</h3>
                         </div>
+                        <div className="main-grid-day-time-container" style={{position: "relative", height: '75%', outline: "1px solid"}}>
+
+                            {Array.from(Array(hourRange[1] - hourRange[0] + 1).keys()).map((i) => {
+                                const total = hourRange[1] - hourRange[0];
+                                return <div key={i} style={{width:'100%', height:'1px', position: 'absolute',
+                                    top: ((i / total) * 100).toString() + "%", left: 0, backgroundColor: "rgb(184,184,184)"}}></div>;
+                            })}
+
+                            <div className="goal-block" style={{
+                                backgroundColor: "rgb(168,244,208)",
+                                paddingLeft: "5px",
+                                paddingRight: "5px",
+                                borderRadius: "5px",
+                                fontFamily: "sans-serif",
+                                height: "25px",
+                                overflow: "hidden",
+                                borderTop: "solid 2px",
+                                borderBottom: "dashed 1px",
+                                borderRight: "dashed 1px",
+                                borderLeft: "dashed 1px",
+                                position: "absolute",
+                                top: "5%",
+                                left: '50%',
+                                transform: 'translateX(-50%)'
+                            }}>
+                                Goal Name
+                            </div>
+
+
+                        </div>
+                        <div className="main-grid-day-overflow-container" style={{position: "relative", height: "20%", outline: "1px solid"}}>
+
+                        </div>
+
                     </div>
                 </div>
 
